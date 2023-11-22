320bcf3c844e5e81bece3216c6b83d12579bd57b
LauraBalasso-2 <laura.balasso@optimad.it>
2023-11-22 16:58:19 +0100

    run on cpu

--- reconstruct.py
+++ reconstruct.py
@@ -35,9 +35,9 @@ def reconstruct(
     adjust_lr_every = int(num_iterations / 2)
 
     if type(stat) == type(0.1):
-        latent = torch.ones(1, latent_size).normal_(mean=0, std=stat).cuda()
+        latent = torch.ones(1, latent_size).normal_(mean=0, std=stat).cpu()
     else:
-        latent = torch.normal(stat[0].detach(), stat[1].detach()).cuda()
+        latent = torch.normal(stat[0].detach(), stat[1].detach()).cpu()
 
     latent.requires_grad = True
 
@@ -51,7 +51,7 @@ def reconstruct(
         decoder.eval()
         sdf_data = deep_sdf.data.unpack_sdf_samples_from_ram(
             test_sdf, num_samples
-        ).cuda()
+        ).cpu()
         xyz = sdf_data[:, 0:3]
         sdf_gt = sdf_data[:, 3].unsqueeze(1)
 
@@ -63,7 +63,7 @@ def reconstruct(
 
         latent_inputs = latent.expand(num_samples, -1)
 
-        inputs = torch.cat([latent_inputs, xyz], 1).cuda()
+        inputs = torch.cat([latent_inputs, xyz], 1).cpu()
 
         pred_sdf = decoder(inputs)
 
@@ -143,7 +143,7 @@ if __name__ == "__main__":
     deep_sdf.configure_logging(args)
 
     def empirical_stat(latent_vecs, indices):
-        lat_mat = torch.zeros(0).cuda()
+        lat_mat = torch.zeros(0).cpu()
         for ind in indices:
             lat_mat = torch.cat([lat_mat, latent_vecs[ind]], 0)
         mean = torch.mean(lat_mat, 0)
@@ -176,7 +176,7 @@ if __name__ == "__main__":
 
     decoder.load_state_dict(saved_model_state["model_state_dict"])
 
-    decoder = decoder.module.cuda()
+    decoder = decoder.module.cpu()
 
     with open(args.split_filename, "r") as f:
         split = json.load(f)
